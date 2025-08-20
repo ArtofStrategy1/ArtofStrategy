693894135324449c20bdbfc63cc7da1758ca6c70
Matthew Christian <matthewchristian33@gmail.com>
2025-08-20 05:53:38 +0000

    Added a function which constructs a NetworkX DiGraph Knowledge Graph using the nodes and edges stored in the database.

--- backend/spacy-nlp/spacy-nlp-app/utils/graph_utils.py
+++ backend/spacy-nlp/spacy-nlp-app/utils/graph_utils.py
@@ -1,3 +1,8 @@
+import networkx as nx
+from typing import List
+from ..models import GraphNode, GraphEdge
+
+
 def find_lca(ent1_root, ent2_root):
     """
     Finds the lowest common ancestor (LCA) of two spaCy tokens in the dependency tree.
@@ -20,3 +25,24 @@ def find_lca(ent1_root, ent2_root):
         else:
             lca = ent1_root
     return lca
+
+
+def get_networkx_graph_from_db_data(
+    nodes: List[GraphNode], edges: List[GraphEdge]
+) -> nx.DiGraph:
+    """
+    Constructs a NetworkX DiGraph from a list of GraphNode and GraphEdge objects.
+    """
+    graph = nx.DiGraph()
+
+    for node in nodes:
+        graph.add_node(
+            node.id, label=node.label, type=node.type, properties=node.properties
+        )
+
+    for edge in edges:
+        graph.add_edge(
+            edge.source, edge.target, relation=edge.label, properties=edge.properties
+        )
+
+    return graph
