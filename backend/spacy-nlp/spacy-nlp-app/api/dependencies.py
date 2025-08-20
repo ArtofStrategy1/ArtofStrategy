6a4524aa2d085bf99e90fac3bfc75a543c035815
Matthew Christian <matthewchristian33@gmail.com>
2025-08-20 05:37:34 +0000

    Defined a function used for dependency injection of the asynchronous database session.

--- backend/spacy-nlp/spacy-nlp-app/api/dependencies.py
+++ backend/spacy-nlp/spacy-nlp-app/api/dependencies.py
@@ -1,5 +1,13 @@
 from fastapi import Request
+from ..database.connection import get_db
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 def get_nlp_model(request: Request):
     return request.app.state.nlp
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async for db_session in get_db():
+        yield db_session
