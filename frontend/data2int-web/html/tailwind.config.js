e05db9d20ecebddd539cb6857635c8e931d34f10
Matthew Christian <matthewchristian33@gmail.com>
2025-12-03 17:22:42 +0000

    Added local Tailwind CSS library.

---
+++ frontend/data2int-web/html/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./js//*.js"], // Scans index.html and any JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
